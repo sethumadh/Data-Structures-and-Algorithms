5bdcf936ee02dd9f6d95a8fd1414a22e5ef60949
sethu <sethumadh@gmail.com>
2023-03-02 10:41:07 +1000

    fabonacci update

--- fabonacci.js
+++ fabonacci.js
@@ -16,30 +16,5 @@ const fab = (n) => {
 console.log(fab(10))
 
 
-// const primenumber = (n) => {
-//   if (n === 0) {
-//     return ` division with zero not allowed`
-//   }
-//   for (let i = 2; i < Math.sqrt(n); i++) {
-//     if (n % i === 0) {
-//       return false
-//     }
-//   }
-//   return true
-// }
-// console.log(primenumber(10))
 
-//find a number is a power of 2
-
-    // function twoRoot(n) {
-    //   let i = 1
-    //   while (n > 1) {
-    //     if (n % 2 !== 0) {
-    //       return false
-    //     }
-    //     n = n / 2
-    //   }
-    //   return true
-    // }
-
-    // console.log(twoRoot(8))
+// Big-0=O(n)
