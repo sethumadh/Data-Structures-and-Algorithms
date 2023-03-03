935a6f166f3b9379137bbbea70e4479d08777c05
sethu <sethumadh@gmail.com>
2023-03-03 10:32:58 +1000

    add powerRoot2

---
+++ powerRoot2.js
@@ -0,0 +1,16 @@
+//find a number is a power of 2
+
+function twoRoot(n) {
+  let i = 1
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false
+    }
+    n = n / 2
+  }
+  return true
+}
+
+console.log(twoRoot(8))
+
+// big-O=log(n)
